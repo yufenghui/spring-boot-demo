15691906101e6847c5a51f6261f7ae97e9c6d9f4
于冯辉 <yufenghui@vchangyi.com>
2022-11-23 18:33:19 +0800

    user: add service module

---
+++ src/main/java/com/yufenghui/boot/demo/service/UserService.java
@@ -0,0 +1,14 @@
+package com.yufenghui.boot.demo.service;
+
+/**
+ * UserService
+ * <p/>
+ *
+ * @author yufenghui
+ * @date 2022/11/23 18:31
+ */
+public interface UserService {
+
+    void getUser();
+
+}
---
+++ src/main/java/com/yufenghui/boot/demo/service/impl/UserServiceImpl.java
@@ -0,0 +1,29 @@
+package com.yufenghui.boot.demo.service.impl;
+
+import com.yufenghui.boot.demo.service.UserService;
+import org.springframework.stereotype.Service;
+
+/**
+ * UserServiceImpl
+ * <p/>
+ *
+ * @author yufenghui
+ * @date 2022/11/23 18:32
+ */
+@Service
+public class UserServiceImpl implements UserService {
+    
+    @Override
+    public void getUser() {
+
+        for (int i = 0; i < 10; i++) {
+
+            for (int j = 0; j < 10; j++) {
+                
+            }
+            
+        }
+        
+    }
+    
+}
