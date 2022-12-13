14512ca2d0e368d62f9a59c48997e17b6e8fa726
于冯辉 <yufenghui@vchangyi.com>
2022-12-13 17:07:33 +0800

    java: change for loop code

--- src/main/java/com/yufenghui/boot/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/yufenghui/boot/demo/service/impl/UserServiceImpl.java
@@ -18,8 +18,6 @@ public class UserServiceImpl implements UserService {
 
         for (int i = 0; i < 10; i++) {
 
-            System.out.println("xxxxxxxxxxx");
-
             for (int j = 0; j < 10; j++) {
                 System.out.println("xxxxxxxxxxx");
             }
